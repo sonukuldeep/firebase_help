40357168cdae91d34dfaa8c06a542c3374cb2403
sonukuldeep <sonukuldeep@outlook.com>
2022-10-23 13:02:04 +0530

    rtdb functional now

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
-import {addtoDatabase,addToRealTimeDB, updateRealTimeDB, signUp} from './Assets/firebaseConfig.js'
+import {addtoDatabase,addToRealTimeDB, updateRealTimeDB, signUp, logIn, logout} from './Assets/firebaseConfig.js'
 
-// document.getElementById("myForm").addEventListener("submit", submitForm)
-function submitForm(e){
-    e.preventDefault()
-    let name = e.target.elements.name.value
-    let email = e.target.elements.email.value
-    let msg = e.target.elements.msg.value
-
-    const data = {"name": name,"email":email,"msg":msg}
-    // console.log(data)
-    // addtoDatabase(data)
-    addToRealTimeDB(data)
-    // updateRealTimeDB(data)
-  }
-
-document.getElementById("SigninForm").addEventListener("submit", signInForm)
-function signInForm(e) {
+//signup signin and logout
+document.getElementById("SignUpForm").addEventListener("submit", signUp_Form)
+function signUp_Form(e) {
   e.preventDefault()
   let email = e.target.elements.email.value
   let passwd = e.target.elements.passwd.value
   signUp(email,passwd)
 }
 
-const lis = document.querySelectorAll('li')
+document.getElementById("SigninForm").addEventListener("submit", signoutForm)
+function signoutForm(e) {
+  e.preventDefault()
+  let email = e.target.elements.email.value
+  let passwd = e.target.elements.passwd.value
+  logIn(email,passwd)
+}
+
+document.getElementById("signout-btn-1").addEventListener("click", ()=>{logout()})
+document.getElementById("signout-btn-2").addEventListener("click", ()=>{logout()})
+//xxxx
+
+//RTDBAdd
+document.getElementById("RTDBFormAdd").addEventListener("submit", RTDBAdd)
+function RTDBAdd(e) {
+  e.preventDefault()
+  const name = e.target.elements.name.value
+  const msg = e.target.elements.msg.value
+  const data = {"name": name, "msg": msg}
+  addToRealTimeDB(data)
+}
+
+//RTDBupdate
+document.getElementById("RTDBFormUpdate").addEventListener("submit", RTDBUpdate)
+function RTDBUpdate(e) {
+  e.preventDefault()
+  const name = e.target.elements.name.value
+  const msg = e.target.elements.msg.value
+  const data = {"name": name, "msg": msg}
+  updateRealTimeDB(data)
+}
+
+//RTDBupdate
+document.getElementById("RTDBFormDelete").addEventListener("submit", RTDBDeleteNode)
+function RTDBDeleteNode(e) {
+  e.preventDefault()
+  updateRealTimeDB(null)
+}
+
+const lis = document.querySelectorAll('.trigger')
 const forms = ["SignUpForm", "SigninForm", "RTDBFormAdd", "RTDBFormRead", "RTDBFormUpdate", "RTDBFormDelete", "CDBFormAdd", "CDBFormRead", "CDBFormUpdate", "CDBFormDelete"]
 lis.forEach((li, index)=>{li.addEventListener("click", ()=>{invokeComponent(forms[index])})})
 
@@ -32,7 +58,6 @@ function invokeComponent(id) {
   {
     const form = document.getElementById(forms[count])
     form.style.display = "none"
-    // console.log(form.style,forms[count],count)
     count +=1
   }
   const form = document.getElementById(id)
